260007750b865ff9567afa15a6f03e41a5bb6a98
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2025-06-25 07:51:39 -0400

    Update install.sh
    Stronger syntax for more reliable execution

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 #shellcheck disable=all
+
+#Source: https://cwiki.apache.org/confluence/display/openmeetings/tutorials+for+installing+openmeetings+and+tools?preview=/27838216/334761243/Installation%20OpenMeetings%208.0.0%20on%20Ubuntu%2024.04%20lts.pdf
  
 log_file="$PWD/server_install.log"
 
@@ -17,12 +19,22 @@ echo "updating system..."
 update
 
 required_install() {
-    #nano install 
-    sudo apt-get install nano -y  &&
-    #java install
-    sudo apt-get install openjdk-21-jre openjdk-21-jre-headless -y 
+#nano install 
+#sudo apt-get install nano -y  &&
+update
+#java install
+#sudo apt-get install openjdk-21-jre openjdk-21-jre-headless -y 
+#
+yes y | sudo add-apt-repository ppa:libreoffice/ppa &&
+update  &&
+sudo apt-get install libreoffice -y &&
+update &&
+sudo apt-get install imagemagick libjpeg62 zlib1g-dev -y &&
+update &&
+sudo apt-get install sox -y && 
+update 
 }
 
 echo "installing requirements..."
 
-required_install || error 
+{ required_install || { error; } } | tee -a "$log_file" 
