2868c60cd7133ed799816a3315f25184b562a6ff
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2025-06-27 22:53:19 -0400

    Update install.sh
    More modular method implemented - with each component offering confirmation after a successful execution. This version makes it easier to track success and failure throughout the install.

--- install.sh
+++ install.sh
@@ -15,26 +15,90 @@ error() { echo "Error. Abort!" && exit 1; }
 #ffmpeg, MariaDB
 
 #update before installing packages 
-echo "updating system..." 
+echo "updating system. Preparing for install..." 
 update
 
-required_install() {
-#nano install 
-#sudo apt-get install nano -y  &&
-update
-#java install
-#sudo apt-get install openjdk-21-jre openjdk-21-jre-headless -y 
-#
+echo "installing nano..." 
+
+if sudo apt-get install nano -y || error; then 
+    echo "successfully installed nano" 
+fi
+
+echo "installing java components..."  
+
+if sudo apt-get install openjdk-21-jre openjdk-21-jre-headless -y || error; then 
+    echo "successfully installed java packages" 
+fi
+
+
+libre_install() {
+#Install libreoffice ppa 
 yes y | sudo add-apt-repository ppa:libreoffice/ppa &&
-update  &&
-sudo apt-get install libreoffice -y &&
-update &&
-sudo apt-get install imagemagick libjpeg62 zlib1g-dev -y &&
-update &&
-sudo apt-get install sox -y && 
-update 
+echo "succcessfully added libre office repository"
+#Install libre office 
+sudo apt-get install libreoffice -y  && 
+echo "successfully installed libre office packages"
 }
 
-echo "installing requirements..."
+echo "installing libre office components..." 
 
-{ required_install || { error; } } | tee -a "$log_file" 
+if libre_install || error; then 
+   echo "finished installing libre office" 
+fi 
+
+echo "installing imagemagick..." 
+
+if sudo apt-get install imagemagick libjpeg62 zlib1g-dev -y || error; then 
+   echo "successfully installed imagemagick" 
+fi 
+
+echo "installing sox for audio..." 
+
+if sudo apt-get install sox -y || error; then 
+    echo "finished installing sox" 
+fi
+
+
+echo "installing ffmpeg and curl..." 
+
+if sudo apt-get install ffmpeg vlc curl -y || error; then 
+    echo "successfully installed ffmpeg and curl" 
+fi
+
+echo "installing MariaDB database server..." 
+
+if sudo apt-get install mariadb-server -y || error; then 
+    echo "server installed - starting daemon..." 
+    if sudo systemctl enable --now mariadb; then
+       echo "successfully started server daemon"
+   else 
+       error 
+    fi
+fi
+
+echo "finished!"
+
+#required_install() {
+#update system
+#update
+#java install
+#yes y | sudo add-apt-repository ppa:libreoffice/ppa &&
+#update system
+#update  &&
+##Install libre office 
+#sudo apt-get install libreoffice -y &&
+#update system
+#update &&
+#Install imagemagick
+#sudo apt-get install imagemagick libjpeg62 zlib1g-dev -y &&
+#update system
+#update &&
+#Install sox for audio
+#sudo apt-get install sox -y && 
+#update the system
+#update 
+#}
+
+#echo "installing requirements..."
+
+#{ required_install || { error; } } | tee -a "$log_file" 
